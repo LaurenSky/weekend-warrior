e258d31459221dd3ab525adb5f833c4193c1cf9e
LaurenSky <ljherrer@gmail.com>
2016-08-22 17:22:34 -0700

    hello_world all test pass 2 ways

--- 00-hello-world/hello_world.rb
+++ 00-hello-world/hello_world.rb
@@ -1,3 +1,11 @@
+# def hello_world(name = "World!")
+#   if name == ""
+#     return "Hello, World!"
+#   else
+#     return "Hello, #{ name }!"
+#   end
+# end
+
 def hello_world(name = "")
   name = "World" if name == ""
     return "Hello, #{ name }!"
